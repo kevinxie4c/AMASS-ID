2149ce8c9087e3f304a1c5bff846d85f9e128be0
Kaixiang Xie <kaixiang.xie@mail.mcgill.ca>
2022-07-20 17:21:51 -0400

    inverse.pl add some options

--- inverse.pl
+++ inverse.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-use Getopt::Long;
+use Getopt::Long qw(:config no_ignore_case);
 use strict;
 use warnings;
 
@@ -10,14 +10,22 @@ my $ground_offset = 0;
 my $reg = 0.0001;
 my $outdir = "output";
 my $pose_file;
+my @opts;
+
+sub add_opts {
+    push @opts, "-$_[0]", $_[1];
+}
 
 GetOptions(
-    "j|char_file=s" => \$char_file,
-    "f|frame_time=i" => \$frame_time,
-    "c|cutoff_freq=i" => \$cutoff_freq,
-    "g|ground_offset=i" => \$ground_offset,
-    "r|regularization=i" => \$reg,
-    "o|outdir=s"=> \$outdir,
+    "j|char_file=s" => \&add_opts,
+    "f|frame_time=i" => \&add_opts,
+    "c|cutoff_freq=i" => \&add_opts,
+    "u|mu=f" => \&add_opts,
+    "s|step_length=i" => \&add_opts,
+    "r|regularization=i" => \&add_opts,
+    "o|outdir=s"=> sub { push @opts, "-$_[0]", $_[1]; $outdir = $_[1] },
+    "A|start_frame=i" => \&add_opts,
+    "E|end_frame=i" => \&add_opts,
 );
 
 if (@ARGV != 1) {
@@ -28,9 +36,12 @@ options:
     -j, --char_file=string
     -f, --frame_time=double
     -c, --cutoff_freq=double
-    -g, --ground_offset=double
+    -u, --mu=double
+    -s, --step_length=int
     -r, --regularization=double
     -o, --outdir=string
+    -A, --start_frame=int
+    -E, --end_frame=int
 USAGE
 } else {
     $pose_file = shift @ARGV;
@@ -38,10 +49,10 @@ USAGE
 
 mkdir $outdir unless -e $outdir;
 
-my $cmd;
-$cmd = "python find-contact-node.py '$pose_file' '$outdir'";
-print "$cmd\n";
-system $cmd;
-$cmd = "./inverse.out --char_file='$char_file' --frame_time=$frame_time --cutoff_freq=$cutoff_freq --ground_offset=$ground_offset --regularization=$reg --outdir='$outdir' '$pose_file' '$outdir/contact_nodes.txt'";
-print "$cmd\n";
-system $cmd;
+my @cmd;
+@cmd = ('python', 'find-contact-node.py', $pose_file, $outdir);
+print "@cmd\n";
+system @cmd;
+@cmd = ('./inverse.out', @opts, $pose_file, "$outdir/contact_nodes.txt");
+print "@cmd\n";
+system @cmd;
