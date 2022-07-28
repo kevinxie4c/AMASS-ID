9251c0000f04a23ee19f366a97ea07777ff242b7
Kaixiang Xie <kaixiang.xie@mail.mcgill.ca>
2022-07-28 04:36:01 -0400

    show both sim and reference

--- src/CharacterViewer.cpp
+++ src/CharacterViewer.cpp
@@ -23,9 +23,8 @@ float meshAlpha = 0.3;
 bool inverted = false;
 bool transparent = true;
 bool playing = false;
-bool is_npz = false;
 Eigen::MatrixXd positions;
-dart::dynamics::SkeletonPtr skeleton;
+dart::dynamics::SkeletonPtr skeleton = nullptr;
 osg::ref_ptr<osg::Geometry> mesh;
 std::vector<Eigen::MatrixXd> vertices;
 std::vector<Eigen::VectorXd> pointIndices;
@@ -104,39 +103,34 @@ public:
 	ImGui::Checkbox("Transparent mesh", &transparent);
 	ImGui::SliderFloat("Alpha", &meshAlpha, 0.0f, 1.0f);
 	ImGui::End();
-	if (is_npz)
+	if (transparent)
 	{
-	    if (transparent)
-	    {
-		stateSet->setMode(GL_DEPTH_TEST, osg::StateAttribute::OFF | osg::StateAttribute::OVERRIDE);
-		stateSet->setMode(GL_BLEND, osg::StateAttribute::ON | osg::StateAttribute::OVERRIDE);
-	    }
-	    else
-	    {
-		stateSet->setMode(GL_DEPTH_TEST, osg::StateAttribute::ON | osg::StateAttribute::OVERRIDE);
-		stateSet->setMode(GL_BLEND, osg::StateAttribute::OFF | osg::StateAttribute::OVERRIDE);
-	    }
+	    stateSet->setMode(GL_DEPTH_TEST, osg::StateAttribute::OFF | osg::StateAttribute::OVERRIDE);
+	    stateSet->setMode(GL_BLEND, osg::StateAttribute::ON | osg::StateAttribute::OVERRIDE);
 	}
-	skeleton->setPositions(positions.row(frame));
+	else
+	{
+	    stateSet->setMode(GL_DEPTH_TEST, osg::StateAttribute::ON | osg::StateAttribute::OVERRIDE);
+	    stateSet->setMode(GL_BLEND, osg::StateAttribute::OFF | osg::StateAttribute::OVERRIDE);
+	}
+	if (skeleton != nullptr)
+	    skeleton->setPositions(positions.row(frame));
 	if (frame < vertices.size())
 	{
-	    if (is_npz)
+	    osg::Vec3Array* vertexArray = static_cast<osg::Vec3Array*>(mesh->getVertexArray());
+	    osg::Vec4Array* colorArray = static_cast<osg::Vec4Array*>(mesh->getColorArray());
+	    for (int i = 0; i < vertices[0].rows(); ++i)
 	    {
-		osg::Vec3Array* vertexArray = static_cast<osg::Vec3Array*>(mesh->getVertexArray());
-		osg::Vec4Array* colorArray = static_cast<osg::Vec4Array*>(mesh->getColorArray());
-		for (int i = 0; i < vertices[0].rows(); ++i)
-		{
-		    const Eigen::VectorXd &v = vertices[frame].row(i);
-		    (*vertexArray)[i] = osg::Vec3(v[0], v[1], v[2]);
-		    (*colorArray)[i] = osg::Vec4(1.0, 1.0, 1.0, meshAlpha);
-		}
-		for (int i = 0; i < pointIndices[frame].rows(); ++i)
-		{
-		    (*colorArray)[pointIndices[frame][i]] = osg::Vec4(1.0, 0.0, 0.0, meshAlpha);
-		}
-		mesh->dirtyDisplayList();
-		mesh->dirtyBound();
+		const Eigen::VectorXd &v = vertices[frame].row(i);
+		(*vertexArray)[i] = osg::Vec3(v[0], v[1], v[2]);
+		(*colorArray)[i] = osg::Vec4(1.0, 1.0, 1.0, meshAlpha);
 	    }
+	    for (int i = 0; i < pointIndices[frame].rows(); ++i)
+	    {
+		(*colorArray)[pointIndices[frame][i]] = osg::Vec4(1.0, 0.0, 0.0, meshAlpha);
+	    }
+	    mesh->dirtyDisplayList();
+	    mesh->dirtyBound();
 	    /*
 	    group->removeChildren(0, group->getNumChildren());
 	    for (size_t i = 0; i < contacts[frame].rows(); i += 6)
@@ -205,16 +199,17 @@ public:
 
 void printUsage(char * prgname)
 {
-    std::cout << "usage: " << prgname << " pose_file contact_point_indices_file contact_forces_file" << std::endl;
+    std::cout << "usage: " << prgname << " AMASS_pose_file contact_point_indices_file contact_forces_file" << std::endl;
     std::cout << std::endl;
     std::cout << "options:" << std::endl;
     std::cout << "\t-j, --char_file=string" << std::endl;
-    std::cout << "\t-z, --npz" << std::endl;
+    std::cout << "\t-p, --pose_file=string" << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
     std::string jsonFilename = "data/character.json";
+    std::string amassFilename;
     std::string poseFilename;
     std::string cpiFilename;
     std::string cfFilename;
@@ -225,12 +220,12 @@ int main(int argc, char *argv[])
 	static struct option long_options[] =
 	{
 	    { "char_file", required_argument, NULL, 'j' },
-	    { "npz", required_argument, NULL, 'z' },
+	    { "pose_file", required_argument, NULL, 'p' },
 	    { 0, 0, 0, 0 }
 	};
 	int option_index = 0;
 
-	c = getopt_long(argc, argv, "j:z", long_options, &option_index);
+	c = getopt_long(argc, argv, "j:p:", long_options, &option_index);
 	if (c == -1)
         break;
 
@@ -239,8 +234,8 @@ int main(int argc, char *argv[])
 	    case 'j':
 		jsonFilename = optarg;
 		break;
-	    case 'z':
-		is_npz = true;
+	    case 'p':
+		poseFilename = optarg;
 		break;
 	    default:
 		printUsage(argv[0]);
@@ -250,7 +245,7 @@ int main(int argc, char *argv[])
 
     if (optind + 3 == argc)
     {
-	poseFilename = argv[optind];
+	amassFilename = argv[optind];
 	cpiFilename = argv[optind + 1];
 	cfFilename = argv[optind + 2];
     }
@@ -266,158 +261,157 @@ int main(int argc, char *argv[])
 
     pointIndices = readVectorXdListFrom(cpiFilename);
 
-    if (is_npz)
-    {
-	//Py_SetProgramName(argv[0]);
-	Py_Initialize();
-	//PySys_SetArgv(argc, argv);
-	PyRun_SimpleString("import sys\n"
-			   "sys.path.append(\".\")\n"
-			   "print('python version:', sys.version)\n");
+    //Py_SetProgramName(argv[0]);
+    Py_Initialize();
+    //PySys_SetArgv(argc, argv);
+    PyRun_SimpleString("import sys\n"
+		       "sys.path.append(\".\")\n"
+		       "print('python version:', sys.version)\n");
 
-	std::cout << "loading mesh..." << std::endl;
-	const char *module = "amass_mesh";
-	const char *func = "AMASSmesh";
-	PyObject *pName, *pModule, *pFunc, *pValue, *pArgs;
-	pName = PyUnicode_FromString(module);
-	pModule = PyImport_Import(pName);
-	Py_DECREF(pName);
-	if (pModule != NULL)
+    std::cout << "loading mesh..." << std::endl;
+    const char *module = "amass_mesh";
+    const char *func = "AMASSmesh";
+    PyObject *pName, *pModule, *pFunc, *pValue, *pArgs;
+    pName = PyUnicode_FromString(module);
+    pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
+    if (pModule != NULL)
+    {
+	pFunc = PyObject_GetAttrString(pModule, func);
+	if (pFunc && PyCallable_Check(pFunc))
 	{
-	    pFunc = PyObject_GetAttrString(pModule, func);
-	    if (pFunc && PyCallable_Check(pFunc))
-	    {
-		pValue = PyUnicode_FromString(poseFilename.c_str());
-		pArgs = PyTuple_New(1);
-		PyTuple_SetItem(pArgs, 0, pValue);
-		pValue = PyObject_CallObject(pFunc, pArgs);
-		Py_DECREF(pArgs);
-		Py_DECREF(pFunc);
-		Py_DECREF(pModule);
-	    }
-	    else
-	    {
-		if (PyErr_Occurred())
-		    PyErr_Print();
-		fprintf(stderr, "failed to call function \"%s\"\n", func);
-		return 0;
-	    }
-	    Py_XDECREF(pFunc);
+	    pValue = PyUnicode_FromString(amassFilename.c_str());
+	    pArgs = PyTuple_New(1);
+	    PyTuple_SetItem(pArgs, 0, pValue);
+	    pValue = PyObject_CallObject(pFunc, pArgs);
+	    Py_DECREF(pArgs);
+	    Py_DECREF(pFunc);
 	    Py_DECREF(pModule);
 	}
 	else
 	{
-	    PyErr_Print();
-	    fprintf(stderr, "failed to load \"%s\"\n", module);
+	    if (PyErr_Occurred())
+		PyErr_Print();
+	    fprintf(stderr, "failed to call function \"%s\"\n", func);
 	    return 0;
 	}
-	PyObject *pFaces, *pVertices;
-	pFaces = PyObject_GetAttrString(pValue, "faces");
-	pVertices = PyObject_GetAttrString(pValue, "vertices");
-	Eigen::MatrixXd faces = npa2mat(pFaces);
-	vertices = npa2mat_vector(pVertices);
-	//std::cout << faces.row(0) << std::endl;
-	//std::cout << vertices[0].row(0) << std::endl;
-	Py_DECREF(pFaces);
-	Py_DECREF(pVertices);
-	Py_DECREF(pValue);
-	std::cout << "mesh loaded" << std::endl;
-	/*
-	pDict = load_npz("data/0005_Walking001_mesh.npz");
-	npa = PyMapping_GetItemString(pDict, "faces");
-	Eigen::MatrixXd faces = npa2mat(npa);
-	npa = PyMapping_GetItemString(pDict, "vertices");
-	std::vector<Eigen::MatrixXd> vertices = npa2mat_vector(npa);
-	Py_DECREF(pDict);
-	std::cout << faces.row(0) << std::endl;
-	std::cout << vertices[0].row(0) << std::endl;
-	*/
-
-	PyObject *pDict = load_npz(poseFilename);
-	//print_py_obj(pDict);
-	PyObject *npa;
-	npa = PyMapping_GetItemString(pDict, "trans");
-	//print_py_obj(npa);
-	Eigen::MatrixXd trans = npa2mat(npa);
-	//Py_DECREF(npa);
-	//std::cout << trans.row(0) << std::endl;
-	npa = PyMapping_GetItemString(pDict, "poses");
-	Eigen::MatrixXd poses_orig = npa2mat(npa);
-	//Py_DECREF(npa);
-	//std::cout << poses_orig.row(0) << std::endl;
-	Py_DECREF(pDict);
-
-	std::vector<size_t> indices{0, 3, 12, 21, 30, 6, 15, 24, 33, 9, 18, 27, 36, 45, 39, 48, 54, 60, 42, 51, 57, 63};
-	Eigen::MatrixXd poses(poses_orig.rows(), indices.size() * 3);
-	for (size_t i = 0; i < indices.size(); ++i)
-	    poses.middleCols(i * 3, 3) = poses_orig.middleCols(indices[i], 3);
-
-	positions = Eigen::MatrixXd(poses.rows(), poses.cols() + 3);
-	positions.leftCols(3) = poses.leftCols(3);
-	positions.middleCols(3, 3) = trans;
-	positions.rightCols(poses.cols() - 3) = poses.rightCols(poses.cols() - 3);
-
-	osg::ref_ptr<osg::Vec3Array> vertexArray = new osg::Vec3Array();
-	osg::ref_ptr<osg::Vec4Array> colorArray = new osg::Vec4Array();
-	osg::ref_ptr<osg::DrawElementsUInt> indexArray = new osg::DrawElementsUInt(GL_TRIANGLES);
-	for (int i = 0; i < vertices[0].rows(); ++i)
-	{
-	    const Eigen::VectorXd &v = vertices[0].row(i);
-	    vertexArray->push_back(osg::Vec3(v[0], v[1], v[2]));
-	    colorArray->push_back(osg::Vec4(1.0, 1.0, 1.0, meshAlpha));
-	}
-	for (int i = 0; i < pointIndices[0].rows(); ++i)
-	{
-	    (*colorArray)[pointIndices[0][i]] = osg::Vec4(1.0, 0.0, 0.0, meshAlpha);
-	}
-	for (int i = 0; i < faces.rows(); ++i)
-	{
-	    const Eigen::VectorXd &v = faces.row(i);
-	    indexArray->push_back(v[0]);
-	    indexArray->push_back(v[1]);
-	    indexArray->push_back(v[2]);
-	}
-	mesh = new osg::Geometry();
-	mesh->setVertexArray(vertexArray.get());
-	mesh->setColorArray(colorArray.get());
-	mesh->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
-	mesh->addPrimitiveSet(indexArray.get());
-	osgUtil::SmoothingVisitor::smooth(*mesh);
-	mesh->setDataVariance(osg::Object::DYNAMIC);
-	osg::ref_ptr<osg::Geode> geoNode = new osg::Geode();
-	geoNode->addDrawable(mesh.get());
-
-	//osg::ref_ptr<osg::StateSet> stateSet = geoNode->getOrCreateStateSet();
-	stateSet = geoNode->getOrCreateStateSet();
-	if (transparent)
-	{
-	    stateSet->setMode(GL_DEPTH_TEST, osg::StateAttribute::OFF | osg::StateAttribute::OVERRIDE);
-	    stateSet->setMode(GL_BLEND, osg::StateAttribute::ON | osg::StateAttribute::OVERRIDE);
-	}
-	else
-	{
-	    stateSet->setMode(GL_DEPTH_TEST, osg::StateAttribute::ON | osg::StateAttribute::OVERRIDE);
-	    stateSet->setMode(GL_BLEND, osg::StateAttribute::OFF | osg::StateAttribute::OVERRIDE);
-	}
-	stateSet->setMode(GL_CULL_FACE, osg::StateAttribute::ON | osg::StateAttribute::OVERRIDE);
-	osg::ref_ptr<osg::BlendFunc> blendFunc = new osg::BlendFunc(osg::BlendFunc::SRC_ALPHA, osg::BlendFunc::ONE_MINUS_SRC_ALPHA);
-	stateSet->setAttributeAndModes(blendFunc.get());
-	osg::ref_ptr<osg::CullFace> cullFace = new osg::CullFace(osg::CullFace::BACK);
-	stateSet->setAttributeAndModes(cullFace.get());
-	osg::ref_ptr<osg::FrontFace> frontFace = new osg::FrontFace(osg::FrontFace::COUNTER_CLOCKWISE);
-	stateSet->setAttributeAndModes(frontFace.get());
-	stateSet->setRenderingHint(osg::StateSet::TRANSPARENT_BIN);
-
-
-	worldNode->addChild(geoNode.get());
+	Py_XDECREF(pFunc);
+	Py_DECREF(pModule);
     }
     else
-	positions = readMatrixXFrom(poseFilename);
+    {
+	PyErr_Print();
+	fprintf(stderr, "failed to load \"%s\"\n", module);
+	return 0;
+    }
+    PyObject *pFaces, *pVertices;
+    pFaces = PyObject_GetAttrString(pValue, "faces");
+    pVertices = PyObject_GetAttrString(pValue, "vertices");
+    Eigen::MatrixXd faces = npa2mat(pFaces);
+    vertices = npa2mat_vector(pVertices);
+    //std::cout << faces.row(0) << std::endl;
+    //std::cout << vertices[0].row(0) << std::endl;
+    Py_DECREF(pFaces);
+    Py_DECREF(pVertices);
+    Py_DECREF(pValue);
+    std::cout << "mesh loaded" << std::endl;
+    /*
+    pDict = load_npz("data/0005_Walking001_mesh.npz");
+    npa = PyMapping_GetItemString(pDict, "faces");
+    Eigen::MatrixXd faces = npa2mat(npa);
+    npa = PyMapping_GetItemString(pDict, "vertices");
+    std::vector<Eigen::MatrixXd> vertices = npa2mat_vector(npa);
+    Py_DECREF(pDict);
+    std::cout << faces.row(0) << std::endl;
+    std::cout << vertices[0].row(0) << std::endl;
+    */
 
-    SimCharacter character(jsonFilename);
-    skeleton = character.skeleton;
-    world->addSkeleton(character.skeleton);
-    character.skeleton->setPositions(positions.row(frame));
+    /*
+    PyObject *pDict = load_npz(amassFilename);
+    //print_py_obj(pDict);
+    PyObject *npa;
+    npa = PyMapping_GetItemString(pDict, "trans");
+    //print_py_obj(npa);
+    Eigen::MatrixXd trans = npa2mat(npa);
+    //Py_DECREF(npa);
+    //std::cout << trans.row(0) << std::endl;
+    npa = PyMapping_GetItemString(pDict, "poses");
+    Eigen::MatrixXd poses_orig = npa2mat(npa);
+    //Py_DECREF(npa);
+    //std::cout << poses_orig.row(0) << std::endl;
+    Py_DECREF(pDict);
+
+    std::vector<size_t> indices{0, 3, 12, 21, 30, 6, 15, 24, 33, 9, 18, 27, 36, 45, 39, 48, 54, 60, 42, 51, 57, 63};
+    Eigen::MatrixXd poses(poses_orig.rows(), indices.size() * 3);
+    for (size_t i = 0; i < indices.size(); ++i)
+	poses.middleCols(i * 3, 3) = poses_orig.middleCols(indices[i], 3);
+
+    positions = Eigen::MatrixXd(poses.rows(), poses.cols() + 3);
+    positions.leftCols(3) = poses.leftCols(3);
+    positions.middleCols(3, 3) = trans;
+    positions.rightCols(poses.cols() - 3) = poses.rightCols(poses.cols() - 3);
+    */
+
+    osg::ref_ptr<osg::Vec3Array> vertexArray = new osg::Vec3Array();
+    osg::ref_ptr<osg::Vec4Array> colorArray = new osg::Vec4Array();
+    osg::ref_ptr<osg::DrawElementsUInt> indexArray = new osg::DrawElementsUInt(GL_TRIANGLES);
+    for (int i = 0; i < vertices[0].rows(); ++i)
+    {
+	const Eigen::VectorXd &v = vertices[0].row(i);
+	vertexArray->push_back(osg::Vec3(v[0], v[1], v[2]));
+	colorArray->push_back(osg::Vec4(1.0, 1.0, 1.0, meshAlpha));
+    }
+    for (int i = 0; i < pointIndices[0].rows(); ++i)
+    {
+	(*colorArray)[pointIndices[0][i]] = osg::Vec4(1.0, 0.0, 0.0, meshAlpha);
+    }
+    for (int i = 0; i < faces.rows(); ++i)
+    {
+	const Eigen::VectorXd &v = faces.row(i);
+	indexArray->push_back(v[0]);
+	indexArray->push_back(v[1]);
+	indexArray->push_back(v[2]);
+    }
+    mesh = new osg::Geometry();
+    mesh->setVertexArray(vertexArray.get());
+    mesh->setColorArray(colorArray.get());
+    mesh->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
+    mesh->addPrimitiveSet(indexArray.get());
+    osgUtil::SmoothingVisitor::smooth(*mesh);
+    mesh->setDataVariance(osg::Object::DYNAMIC);
+    osg::ref_ptr<osg::Geode> geoNode = new osg::Geode();
+    geoNode->addDrawable(mesh.get());
+
+    //osg::ref_ptr<osg::StateSet> stateSet = geoNode->getOrCreateStateSet();
+    stateSet = geoNode->getOrCreateStateSet();
+    if (transparent)
+    {
+	stateSet->setMode(GL_DEPTH_TEST, osg::StateAttribute::OFF | osg::StateAttribute::OVERRIDE);
+	stateSet->setMode(GL_BLEND, osg::StateAttribute::ON | osg::StateAttribute::OVERRIDE);
+    }
+    else
+    {
+	stateSet->setMode(GL_DEPTH_TEST, osg::StateAttribute::ON | osg::StateAttribute::OVERRIDE);
+	stateSet->setMode(GL_BLEND, osg::StateAttribute::OFF | osg::StateAttribute::OVERRIDE);
+    }
+    stateSet->setMode(GL_CULL_FACE, osg::StateAttribute::ON | osg::StateAttribute::OVERRIDE);
+    osg::ref_ptr<osg::BlendFunc> blendFunc = new osg::BlendFunc(osg::BlendFunc::SRC_ALPHA, osg::BlendFunc::ONE_MINUS_SRC_ALPHA);
+    stateSet->setAttributeAndModes(blendFunc.get());
+    osg::ref_ptr<osg::CullFace> cullFace = new osg::CullFace(osg::CullFace::BACK);
+    stateSet->setAttributeAndModes(cullFace.get());
+    osg::ref_ptr<osg::FrontFace> frontFace = new osg::FrontFace(osg::FrontFace::COUNTER_CLOCKWISE);
+    stateSet->setAttributeAndModes(frontFace.get());
+    stateSet->setRenderingHint(osg::StateSet::TRANSPARENT_BIN);
+    worldNode->addChild(geoNode.get());
+
+    if (!poseFilename.empty())
+    {
+	positions = readMatrixXFrom(poseFilename);
+	SimCharacter character(jsonFilename);
+	skeleton = character.skeleton;
+	world->addSkeleton(character.skeleton);
+	character.skeleton->setPositions(positions.row(frame));
+    }
 
     /*
     for (size_t i = 0; i < character.skeleton->getNumDofs(); ++i)
